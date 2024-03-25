a63cffaac4b3d580b358a2283629259da02eb773
liyang <daviderli614@gmail.com>
2024-03-25 18:15:35 +0800

    Update gtdb.rb

--- Formula/gtdb.rb
+++ Formula/gtdb.rb
@@ -1,14 +1,15 @@
 class Greptime < Formula
   desc "An open-source, cloud-native, distributed time-series database with PromQL/SQL/Python supported."
   homepage "https://github.com/GreptimeTeam/greptimedb"
-  version v0.7.1
+  version File.read("version.txt").strip
+
   VERSION = File.read("version.txt").strip
 
   if Hardware::CPU.intel?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.7.1/greptime-darwin-amd64-v0.7.1.tar.gz"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/#{VERSION}/greptime-darwin-amd64-#{VERSION}.tar.gz"
     sha256 "efcf04d1cfb5c521091aa428bec011560d2a743d32df8b8f0c1391f5783f311c"
   elsif Hardware::CPU.arm?
-    url "https://github.com/GreptimeTeam/greptimedb/releases/download/v0.7.1/greptime-darwin-arm64-v0.7.1.tar.gz"
+    url "https://github.com/GreptimeTeam/greptimedb/releases/download/#{VERSION}/greptime-darwin-arm64-#{VERSION}.tar.gz"
     sha256 "cc4bc7823ea69b96a38de26888312998d60c8ded546b87ff9cc8175e6db82cf1"
   end
 
